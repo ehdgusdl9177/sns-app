11f0fb873c79b396ba80fd42bdf50223092c6f89
ehdgusdl9177 <k23129177@gmail.com>
2023-10-10 14:10:14 +0900

    Setup

--- src/server.js
+++ src/server.js
@@ -1,68 +1,77 @@
-const cookieSession = require('cookie-session');
-const express = require('express');
-const { default: mongoose } = require('mongoose');
-const passport = require('passport');
+const cookieSession = require("cookie-session");
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const passport = require("passport");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-const config = require('config');
-const mainRouter = require('./routes/main.router');
-const usersRouter = require('./routes/users.router');
-const serverConfig = config.get('server');
-
+const config = require("config");
+const mainRouter = require("./routes/main.router");
+const usersRouter = require("./routes/users.router");
+const postsRouter = require("./routes/posts.router");
+const commentsRouter = require("./routes/comments.router");
+const profileRouter = require("./routes/profile.router");
+const likesRouter = require("./routes/likes.router");
+const friendsRouter = require("./routes/friends.router");
+const serverConfig = config.get("server");
 
 const port = serverConfig.port;
 
+require("dotenv").config();
 
-require('dotenv').config()
-
-app.use(cookieSession({
-    name: 'cookie-session-name',
-    keys: [process.env.COOKIE_ENCRYPTION_KEY]
-}))
+app.use(
+  cookieSession({
+    name: "cookie-session-name",
+    keys: [process.env.COOKIE_ENCRYPTION_KEY],
+  })
+);
 
 // register regenerate & save after the cookieSession middleware initialization
 app.use(function (request, response, next) {
-    if (request.session && !request.session.regenerate) {
-        request.session.regenerate = (cb) => {
-            cb()
-        }
-    }
-    if (request.session && !request.session.save) {
-        request.session.save = (cb) => {
-            cb()
-        }
-    }
-    next()
-})
+  if (request.session && !request.session.regenerate) {
+    request.session.regenerate = (cb) => {
+      cb();
+    };
+  }
+  if (request.session && !request.session.save) {
+    request.session.save = (cb) => {
+      cb();
+    };
+  }
+  next();
+});
 
 app.use(passport.initialize());
 app.use(passport.session());
-require('./config/passport');
+require("./config/passport");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('mongodb connected')
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.use('/static', express.static(path.join(__dirname, '../public')));
+app.use("/static", express.static(path.join(__dirname, "../public")));
 
-
-app.use('/', mainRouter);
-app.use('/auth', usersRouter);
-
+app.use("/", mainRouter);
+app.use("/auth", usersRouter);
+app.use("/posts", postsRouter);
+app.use("/posts/:id/comments", commentsRouter);
+app.use("/profile/:id", profileRouter);
+app.use("/friends", friendsRouter);
+app.use("/posts:/id/likes", likesRouter);
 
 app.listen(port, () => {
-    console.log(`Listening on ${port}`);
-})
+  console.log(`Listening on ${port}`);
+});
