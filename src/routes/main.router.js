b5327cf0bad8504a0b32521f632a377f760dd932
ehdgusdl9177 <k23129177@gmail.com>
2023-10-11 12:02:00 +0900

    update

--- src/routes/main.router.js
+++ src/routes/main.router.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 const mainRouter = express.Router();
-const { checkAuthenticated, checkNotAuthenticated } = require('../middleware/auth');
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+} = require("../middleware/auth");
 
-mainRouter.get('/', checkAuthenticated, (req, res) => {
-    res.render('index');
+mainRouter.get("/", checkAuthenticated, (req, res) => {
+  res.render("index");
 });
 
-mainRouter.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login');
-})
+mainRouter.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("auth/login");
+});
 
-mainRouter.get('/signup', checkNotAuthenticated, (req, res) => {
-    res.render('signup');
-})
+mainRouter.get("/signup", checkNotAuthenticated, (req, res) => {
+  res.render("auth/signup");
+});
 
 module.exports = mainRouter;
