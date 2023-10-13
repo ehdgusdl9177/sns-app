3e1ed0f306f9985862750d1ae70f99c21204ed4f
ehdgusdl9177 <k23129177@gmail.com>
2023-10-13 10:39:22 +0900

    post router multer

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -1,9 +1,21 @@
 const { request } = require("express");
 const express = require("express");
+const multer = require("multer");
 const { checkAuthenticated } = require("../middleware/auth");
 const Comment = requrie("../models/comments.model");
 const Post = require("../models/posts.model");
 const router = express.Router();
+
+const storageEngine = multer.diskStorage({
+  description: (req, file, callback) => {
+    callback(null, path.join(__dirname, "../public/assets/images"));
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageEngine }).single("image");
 
 router.get("/", checkAuthenticated, (req, res) => {
   Post.findOne()
