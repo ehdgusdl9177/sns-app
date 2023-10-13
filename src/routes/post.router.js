d8827e708ec53ce117e6a20d7841a69a0fc1d4c1
ehdgusdl9177 <k23129177@gmail.com>
2023-10-13 10:23:55 +0900

    post router

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -1,9 +1,23 @@
+const { request } = require("express");
 const express = require("express");
 const { checkAuthenticated } = require("../middleware/auth");
+const Post = require("../models/posts.model");
 const router = express.Router();
 
 router.get("/", checkAuthenticated, (req, res) => {
-  res.redirect("/posts");
+  Post.findOne()
+    .populate("comment")
+    .sort({ createdAt: -1 })
+    .exec((err, posts) => {
+      if (err) {
+        console.log("Error occured");
+      } else {
+        res.render("posts/index", {
+          posts: posts,
+          currentUser: request.user,
+        });
+      }
+    });
 });
 
 module.exports = router;
